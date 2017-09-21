ff3e979d352dcf5e6dc6d35495a03b87666bdde1
Michael J. Stealey <michael.j.stealey@gmail.com>
2017-09-21 09:50:39 -0400

    update scripts

--- database/run-database.sh
+++ database/run-database.sh
@@ -2,5 +2,6 @@
 
 docker-compose build
 docker-compose up -d
+echo "Database is running - use your local IP address for connection"
 
 exit 0;
--- pgadmin4/run-pgadmin4.sh
+++ pgadmin4/run-pgadmin4.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 docker run -d -p 5050:5050 --name pgadmin thajeztah/pgadmin4:latest &
+sleep 3s
+echo "pgAdmin running at http://localhost:5050"
 
 exit 0;
